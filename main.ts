5693d7bc79676bd4dd940c25c2761fd956c0c2e8
coolcoder96 <105137875+coolcoder96@users.noreply.github.com>
2022-09-04 18:54:53 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,128 @@
-
+input.onButtonPressed(Button.A, function () {
+    Play_ani_1()
+})
+function Play_ani_1 () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . # .
+        . . # # .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . . # # .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # #
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . # # # #
+        . # # # #
+        . # # # #
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . # #
+        . # # # #
+        . # # # #
+        . # # # #
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # #
+        . # # # #
+        . # # # #
+        . # # # #
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        . # # # #
+        . # # # #
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+}
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+basic.forever(function () {
+	
+})
